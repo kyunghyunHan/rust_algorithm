cc3d78f6140acf1f0fcdb8f1c390bde2959a4ed1
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-03-10 18:50:55 +0900

    add

--- src/bead_sort.rs
+++ src/bead_sort.rs
@@ -1,4 +1,4 @@
-pub fn bead_sort(a:&mut [usize]){
+pub fn bead_sort(a:&mut [usize])->&mut [usize]{
 
     let mut max= a[0];
     for i in 1..a.len(){
@@ -6,4 +6,26 @@ pub fn bead_sort(a:&mut [usize]){
             max= a[i];
         }
     }
+
+    let mut beads = vec![vec![0; max]; a.len()];
+    for i in 0..a.len() {
+        for j in (0..a[i]).rev() {
+            beads[i][j] = 1;
+        }
+    }
+
+    for j in  0..max{
+        let mut sum =0;
+
+
+        for i in 0..a.len(){
+            sum +=beads[i][j];
+            beads[i][j]=0;
+        }
+        for k in ((a.len()-sum)..a.len()).rev(){
+            a[k]=j+1;
+        }
+    }
+    a
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-
+mod bead_sort;
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut test:[usize;5]= [1,5,5,6,1];
+  let tt=   bead_sort::bead_sort(&mut test);
+    println!("{:?}",tt);
 }
