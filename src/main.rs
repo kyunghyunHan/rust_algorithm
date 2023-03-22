fd30c104c418acbeb58d9674e4a93bc31ad0140a
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-03-22 17:59:07 +0900

    ㅁㅇㅇ

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod DataStructures;
-
+mod Algotithm;
 
 fn main() {
 
-
+  
   DataStructures::merge_sort::main();
   DataStructures::selection_sort::main();
+  Algotithm::fibo::main();
   //   let mut test:[usize;5]= [1,5,5,6,1];
   // let tt=   DataStructures::bead_sort::bead_sort(&mut test);
   //   println!("{:?}",tt);
