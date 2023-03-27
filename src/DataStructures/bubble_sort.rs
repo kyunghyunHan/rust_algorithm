664d46d8275ffae53565de9277a3afad78e529c1
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-03-27 13:06:54 +0900

    ㅁㅇㅇ

---
+++ src/DataStructures/bubble_sort.rs
@@ -0,0 +1,17 @@
+/*버블정렬 */
+fn bubble_sort(a:&[i32],n:i32){
+ for i in 0..n-1{
+
+
+    for j in 0..(n-1)-i{ //왼쪽에서 오른쪽으로
+
+        if a[j]>a[j+1]{
+            //자리바꿈
+        }
+    }
+ }
+}
+
+
+
+pub fn main(){}
