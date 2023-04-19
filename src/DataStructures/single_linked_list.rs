2983e9c3ce97353ffe75ef9a42bf8c96174824d2
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-04-19 23:11:12 +0900

    add

--- src/DataStructures/single_linked_list.rs
+++ src/DataStructures/single_linked_list.rs
@@ -6,14 +6,18 @@
 - 데이터를 추가하거나 삭제하기 위해서는 그 위치에 요소를 삭제한 다음 그 뒤에 있는 모든 원소를 움직여야 한다.
 */
 //단일 연결리스트 노드정의
+
+use std::option::Option;
 struct Node {
     data: i32,
     next: Option<Box<Node>>,
 }
+
 impl Node{
     //새로운 노드생성
-    fn new(data:i32)->Self{
-        Self{data:data,next:None}
+      // 새로운 노드를 생성하는 함수
+      fn new(data: i32) -> Node {
+        Node { data: data, next: None }
     }
 }
 
@@ -27,14 +31,36 @@ impl LinkedList{
         Self { head: None }
     }
     //리스트에 새로운 노드를 추가하는 함수
-    fn push(){
-
+    fn push(&mut self, data: i32) {
+        let mut new_node = Box::new(Node::new(data));
+        match self.head.take() {
+            Some(old_head) => {
+                new_node.next = Some(old_head);
+                self.head = Some(new_node);
+            }
+            None => {
+                self.head = Some(new_node);
+            }
+        }
     }
-     //리스트에서 노드를 제거하고 해당 노드의 데이터를 반환하는 함수
-    fn pop(){}
+
+
+    // 리스트에서 노드를 제거하고 해당 노드의 데이터를 반환하는 함수
+    fn pop(&mut self) -> Option<i32> {
+        match self.head.take() {
+            Some(old_head) => {
+                self.head = old_head.next;
+                Some(old_head.data)
+            }
+            None => None,
+        }
+    }
     
      //리스트가 비어 있는지 확인하는 함수
-    fn is_empty(){}
+    fn is_empty(&self)->bool{
+
+        self.head.is_none()
+    }
 }
 
 pub fn main(){
