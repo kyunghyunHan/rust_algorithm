ce18ed3f00350fe08463a02117bceaf89c34b2fb
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-04-23 21:50:13 +0900

    a

--- src/DataStructures/cirucular_liked_list.rs
+++ src/DataStructures/cirucular_liked_list.rs
@@ -4,17 +4,25 @@
  */
 
 struct Node<T>{
-
+    data:T,
+    next:Option<Box<Node<T>>>,
 }
 
 impl<T>Node<T>{
-
+  //새로운 노드를 생성하는 생성자 
+  fn new(data:T)->Self{
+    Node{data,next:Node}
+  }
 }
 
 struct CirucularLikendList<T>{
-    
+    head:Option<Box<Node<T>>>,
+    tail:Option<Box<Node<T>>>,
 }
 
 impl<T>CirucularLikendList<T>{
+fn new()->Self{
+    CirucularLikendList{head:Nonbe}
+}
     
 }
