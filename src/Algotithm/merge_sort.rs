961d0cd91a769fdd56abd0fa780d3b764aa4632b
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-05-09 22:42:29 +0900

    add

--- src/Algotithm/merge_sort.rs
+++ src/Algotithm/merge_sort.rs
@@ -15,7 +15,18 @@ fn merge_sort(mut arr:&mut [i32]){
     let mut buffer= Vec::with_capacity(arr.len());
     
 }
+fn merge(left:&[i32],right:&[i32],arr:&mut[i32],buffer:&mut[i32]){
 
+    let mut i =0;
+    let mut j= 0;
+    let mut k= 0;
+    while i <left.len()&& j<right.len(){
+
+        if left[i]<=right[j]{
+            //comparison between the left and right partitions
+        }
+    }
+}
 
 fn main(){}
 
