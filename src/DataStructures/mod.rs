140302e795b79c70be67c1a36b017935c5e6a8be
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-05-30 17:33:22 +0900

    add

--- src/DataStructures/mod.rs
+++ src/DataStructures/mod.rs
@@ -1,8 +1,4 @@
-pub mod bead_sort;
-pub mod merge_sort;
-pub mod selection_sort;
-pub mod quick_sort;
-pub mod bubble_sort;
+
 pub mod single_linked_list;
 // pub mod cirucular_liked_list;
 pub mod trie;
---
+++ src/Sort/mod.rs
@@ -0,0 +1,5 @@
+pub mod bead_sort;
+pub mod merge_sort;
+pub mod selection_sort;
+pub mod quick_sort;
+pub mod bubble_sort;
