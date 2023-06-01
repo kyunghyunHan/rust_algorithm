74f28bf7713500029b88d9f00518ec9d232b9004
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-06-01 18:31:27 +0900

    ㅁㅇㅇ

--- src/DataStructures/trie.rs
+++ src/DataStructures/trie.rs
@@ -1,8 +1,13 @@
-/*트리 */
+/*트리 
+최소 신장트리,허프만 코드에 사용
+
+*/
 
 use std::collections::HashMap;
 use std::hash::Hash;
 
+
+/*Node */
 #[derive(Debug, Default)]
 struct Node<Key: Default, Type: Default> {
     children: HashMap<Key, Node<Key, Type>>,
