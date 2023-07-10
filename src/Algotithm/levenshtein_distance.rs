a3c2b92204eb066b495f17730546668fb881d317
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-07-10 18:46:42 +0900

    feat:array

--- src/Algotithm/levenshtein_distance.rs
+++ src/Algotithm/levenshtein_distance.rs
@@ -1,5 +1,36 @@
 /*스트링 편집거리 알고리즘 */
 
+
+
+
+
+fn ed(n:i32,x:&[i32],m:i32,y:&[i32],ins:i32,del:i32,chg:i32){
+    let mut e= vec![vec![0; x.len() + 1]; y.len() + 1];
+    e[0][0]= 0;
+
+    
+    //삽입비용:ins
+    //삭제비용:del,
+    //변경비용:chg
+    for i in 1..=n{
+
+        e[i as usize][0]= e[i as usize-1][0]+del;//첫열의 초기화
+    }
+    for j in 1..=m{
+
+        e[0][j as usize]= e[0][j as usize-1]+ins;//첫행의 초기화
+    }
+    for i in 1..=n{
+        for j in 1..=m{
+            /*
+            c= x[i]==y[j]?0:chg;
+            e[i][j]= min(e[i-1][j]+del,e[i][j-1]+ins,e[i-1][j-1]+c);
+            return e[n][m]
+             */
+        }
+    }
+
+}
 pub fn main(){
     let a = "ca".trim();
     let b = "abc".trim();
