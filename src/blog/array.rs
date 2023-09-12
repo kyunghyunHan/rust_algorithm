f1e8fe8d84961a6acb7d728a25b1855237ba524a
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-09-12 13:31:56 +0900

    feat:blog first

--- src/blog/array.rs
+++ src/blog/array.rs
@@ -1,6 +1,24 @@
 /* array 
 
+Why should I learn data structures?
+
+Data structures aim to efficiently use memory while processing data quickly and reliably. Since data structures are designed to be useful in specific situations, they can be slow and unreliable in other situations. Therefore, the ability to choose the right data structure for a given situation is crucial.
+
+Algorithms, on the other hand, aim to solve specific problems with memory-efficient and fast performance. They are a formalized representation of a set of procedures or methods, often expressed in a mathematical form. In other words, algorithms can be mathematically defined. Like data structures, algorithms can be highly effective in specific situations but may not be suitable in others.
+
+
+
+
 The first data structure in an array.
+
+an array is a collection of data that is stored sequentially in contiguous memory space
+in most programming languages.it typically stores data of the same data type.For example,
+if an array is of type "int,"it can only store integer elements,and elements of other types like double,float.char,etc.,cannot be stored in it.
+
+Each individual value that makes up an array is called an "element",and the number that indicates the position of an elements within the array is referred to as the "index"
+
+
+
 - A linear datga structure that stoires multiple elements of the same data type.
 - Data is stored sequentially in contiguous memory space.
 - Arrays have a fixed size when declared,and this size cannot be changed.
@@ -9,15 +27,17 @@ The first data structure in an array.
 - indices start at O,and the last index is equal to the number of elements in the array minusone.
 - Time complexity for accessing an element is O(1).
 - To insert data into an array,existing data must be shifted,resulting in a time complexity of O(n)
+
+
 - Advantages:
--  - Easy to implement
-- - Memory management is straightforward due to contiguous stroage.
-- - Fast for searching
+- Easy to implement
+- Memory management is straightforward due to contiguous stroage.
+- Fast for searching
 - Disadvantages:
-- - Array size cannot be changed 
-- - Memory wastage can occur.
+-  Array size cannot be changed 
+-  Memory wastage can occur.
 
-
+ㅇㅇdd
 */
 
 struct Array {
@@ -78,4 +98,5 @@ pub fn main(){
     arr.add(1,1);
     arr.add(1,2);
     arr.print();
+    
 }   
