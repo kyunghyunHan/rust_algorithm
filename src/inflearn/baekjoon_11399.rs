68acabf09da2af3229c573f3cfe7c6cace5d91f5
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-09-15 14:16:14 +0900

    feat:merger

---
+++ src/inflearn/baekjoon_11399.rs
@@ -0,0 +1,4 @@
+use std::io::{BufReader,BufRead,BufWriter,Writer,stdin,stdout};
+pub fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+}
--- src/inflearn/mod.rs
+++ src/inflearn/mod.rs
@@ -1,2 +1,3 @@
 pub mod all_sum;
 pub mod baekjoon_17198;
+pub mod baekjoon_11399;
