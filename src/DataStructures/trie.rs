21f8a89184f3543ff04863659ae53ff6485c941c
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-10-17 11:06:03 +0900

    feat:test

--- src/DataStructures/trie.rs
+++ src/DataStructures/trie.rs
@@ -1,69 +1,100 @@
 /*트리 
 최소 신장트리,허프만 코드에 사용
-
+Root: Tree구조의 최상단 Node
+Edge :Node와 Node의 연결
 */
 
-use std::rc::Rc;
-use std::cell::RefCell;
+use std::cmp::Ordering;
 
-#[derive(Debug)]
-pub struct Node {
+struct Node {
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
     data: i32,
-    left_node: Option<Rc<RefCell<Node>>>,
-    right_node: Option<Rc<RefCell<Node>>>,
 }
 
 impl Node {
     fn new(data: i32) -> Self {
         Node {
+            left: None,
+            right: None,
             data,
-            left_node: None,
-            right_node: None,
         }
     }
 
-    fn set_left_node(&mut self, node: Option<Rc<RefCell<Node>>>) {
-        self.left_node = node;
-    }
-
-    fn set_right_node(&mut self, node: Option<Rc<RefCell<Node>>>) {
-        self.right_node = node;
-    }
-
-    fn get_left_node(&self) -> Option<Rc<RefCell<Node>>> {
-        self.left_node.clone()
-    }
-
-    fn get_right_node(&self) -> Option<Rc<RefCell<Node>>> {
-        self.right_node.clone()
-    }
-}
-
-struct TreeClass {
-    new_node: Option<Rc<RefCell<Node>>>,
-}
-
-impl TreeClass {
-    fn make_node(&mut self, data: i32, left_node: Option<Rc<RefCell<Node>>>, right_node: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
-        self.new_node = Some(Rc::new(RefCell::new(Node::new(data))));
-        if let Some(node) = self.new_node.clone() {
-            node.borrow_mut().set_left_node(left_node);
-            node.borrow_mut().set_right_node(right_node);
+    fn insert(&mut self, data: i32) {
+        match data.cmp(&self.data) {
+            Ordering::Less => {
+                if let Some(ref mut left) = self.left {
+                    left.insert(data);
+                } else {
+                    self.left = Some(Box::new(Node::new(data)));
+                }
+            }
+            Ordering::Greater => {
+                if let Some(ref mut right) = self.right {
+                    right.insert(data);
+                } else {
+                    self.right = Some(Box::new(Node::new(data)));
+                }
+            }
+            _ => {}
         }
-        self.new_node.clone()
+    }
+
+    fn search(&self, want_word: i32) -> String {
+        match want_word.cmp(&self.data) {
+            Ordering::Less => {
+                if let Some(ref left) = self.left {
+                    left.search(want_word)
+                } else {
+                    format!("{} Not Found", want_word)
+                }
+            }
+            Ordering::Greater => {
+                if let Some(ref right) = self.right {
+                    right.search(want_word)
+                } else {
+                    format!("{} Not Found", want_word)
+                }
+            }
+            _ => format!("{} is Found", self.data),
+        }
+    }
+
+    pub fn print_tree(&self) {
+        if let Some(ref left) = self.left {
+            left.print_tree();
+        }
+        println!("{}", self.data);
+        if let Some(ref right) = self.right {
+            right.print_tree();
+        }
     }
 }
 
 pub fn main() {
-    let mut tc = TreeClass { new_node: None };
+    let mut root = Node::new(10);
+    root.insert(1);
+    root.insert(3);
+    root.insert(6);
+    root.insert(6);
+    root.insert(9);
 
-    let n50 = tc.make_node(50, None, None);
-    let n60 = tc.make_node(60, None, None);
-    let n20 = tc.make_node(20, n50.clone(), n60.clone());
-    let n30 = tc.make_node(30, None, None);
-    let n10 = tc.make_node(10, n20.clone(), n30.clone());
+    root.insert(13);
 
-    print!("{:?}",n10);
+    root.insert(22);
+
+    root.insert(17);
+    root.insert(10);
+    root.insert(2);
+
+
+    println!("{}", root.search(4));
+
+    println!("{}", root.search(17));
+    println!("{}", root.search(22));
+    println!("{:?}",root.print_tree());
+}
 
     /*
      *         10
@@ -73,4 +104,3 @@ pub fn main() {
      * 50   60
      * 이런 구조의 트리 완성 root노드는 n10임
      */
-}
