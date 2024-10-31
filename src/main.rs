4f59414d93cf6a0f8c82e05e30a319e300438466
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-10-31 22:34:19 +0900

    feat:test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use algorithm::{
     data_structure,
     divide_and_conquer::matrix_multiply,
-    sort::{bubble, heap, insertion, merge, select,counting,bucket},
-    utils::horner,
     graph_algorithm::breadth_first_search,
+    sort::{bubble, bucket, counting, heap, insertion, merge, select},
+    utils::horner,
 };
 fn main() {
-    // insertion::example();
+    insertion::example();
 
     // matrix_multiply::example();
     // select::example();
@@ -15,5 +15,5 @@ fn main() {
     // horner::example();
     // counting::example();
     // bucket::example();
-    breadth_first_search::example();
+    // breadth_first_search::example();
 }
--- src/sort/insertion.rs
+++ src/sort/insertion.rs
@@ -5,31 +5,37 @@ insetion sort
 */
 
 fn sort(arr: &mut Vec<i32>, n: usize) {
-    for i in 1..n {//n
+    for i in 1..n {
+        //n
+
         let key = arr[i]; //현재 삽입될 정수 n-1
-        //a[i]를 정렬된 배열 A[1:i-1]에 삽입한다
+                          // println!("{}번째 {}", i, key);
+                          //a[i]를 정렬된 배열 A[1:i-1]에 삽입한다
         let mut j = (i - 1) as i32;
-        //현재 비교 대상이 배열의 시작을 넘어가지 않게 하고 정렬된 부분에서 
+        println!("{}번째 {}", i, key);
+
+        //현재 비교 대상이 배열의 시작을 넘어가지 않게 하고 정렬된 부분에서
         //A[j] >key를 찾고 j가 0에 도달할떄 while문 종료
-        println!("{}",j);
-        println!("비교{}",arr[j as usize]);
-        println!("key{}",key);
-        //비교대상이 음수가 첫번쨰 배열보다 낮으면안대야 하며 비교대상이 키보다 크면 
+        // println!("{}", j);
+        // println!("비교{}", arr[j as usize]);
+        // println!("key{}", key);
+        //비교대상이 음수가 첫번쨰 배열보다 낮으면안대야 하며 비교대상이 키보다 크면
         //내림차순으로 변경할려면 >key => <key 로변경
-        while j >= 0 && arr[j as usize] > key {  
-            println!("이거{}",arr[j as usize]);
-            println!("{}",j + 1);
+        while j >= 0 && arr[j as usize] < key {
+            // println!("이거{}", arr[j as usize]);
+            // println!("{}", j + 1);
+            // println!("{}번째 {}", i, key);
 
             arr[(j + 1) as usize] = arr[j as usize];
-            j -= 1;//왼쪽으로 이동하면서 비교
+            j -= 1; //왼쪽으로 이동하면서 비교
         }
-        println!("j는?{}",j);
+        println!("j는?{}", j);
         //while문이 종료되면 j는 key가 들어갈 위치보다 한칸 왼쪽에 위치
         arr[(j + 1) as usize] = key;
     }
 }
 pub fn example() {
-    let mut arr = vec![5,2,4,6,1,3];
+    let mut arr = vec![100, 76, 85, 93, 98];
     let n = arr.len();
     sort(&mut arr, n);
     println!("{:?}", arr);
