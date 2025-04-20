2970f60a39a06d607cc1425ac37a2b80cb497bd2
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-04-20 22:35:25 +0900

    feat:Test

---
+++ src/conding_test/books/a09.rs
@@ -0,0 +1,8 @@
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+
+pub fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+
+    let mut input = String::new();
+}
--- src/conding_test/books/mod.rs
+++ src/conding_test/books/mod.rs
@@ -5,11 +5,12 @@ pub mod a04;
 pub mod a05;
 pub mod a06;
 pub mod a07;
+pub mod a08;
+pub mod a09;
 pub mod a11;
 pub mod a16;
 pub mod b02;
 pub mod b03;
-pub mod a08;
 pub fn example() {
-    a02::main();
+    a09::main();
 }
