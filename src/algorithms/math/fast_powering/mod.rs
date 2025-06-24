60ccaec07d2dd0c75e9cd35daaa0becd00555f20
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-06-24 23:34:22 +0900

    feat:update

--- src/algorithms/math/fast_powering/mod.rs
+++ src/algorithms/math/fast_powering/mod.rs
@@ -1,3 +1,12 @@
-pub fn example(){
-    
+fn fast_powering(power: i32) -> i32 {
+    if power == 0 {
+        return 1;
+    }
+
+    if power % 2 == 0 {
+        return 1;
+    }
+    1
 }
+
+pub fn example() {}
