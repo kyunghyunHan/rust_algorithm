90bf947d2a6caf83ad5b6b1af71358645962c835
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-07-07 21:35:26 +0900

    feat:update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use algorithm::{algorithms::math, conding_test, data_structure,algorithms::graph};
+use algorithm::{algorithms::{graph, math, np_complete, sort}, conding_test, data_structure};
 
 fn main() {
     /* Math */
 
     // math::prime_factors::example();
-    math::square_root::example();
+    // math::square_root::example();
+
 
     /*Sort */
     // algorithms::sort::bubble::example();
     // algorithms::sort::insertion::example();
     // algorithms::sort::shell::example();
+    sort::bubble::example();
 
     /* Search */
 
