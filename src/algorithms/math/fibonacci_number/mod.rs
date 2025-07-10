422069d8548f048ec3ef5ac2b75eed97ae419e30
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-07-10 23:19:44 +1000

    feat:udpate

--- src/algorithms/math/fibonacci_number/mod.rs
+++ src/algorithms/math/fibonacci_number/mod.rs
@@ -1,3 +1,23 @@
-pub fn example(){
-    
+fn fibonacci(n: i32) -> Vec<i32> {
+    let mut fib_sequence = vec![1];
+    let mut current_value = 1;
+    let mut previos_value = 0;
+
+    if n == 1 {
+        return fib_sequence;
+    }
+
+    let mut iteration_counter = n - 1;
+
+    while iteration_counter != 0 {
+        current_value += previos_value;
+        previos_value = current_value - previos_value;
+
+        fib_sequence.push(current_value);
+        iteration_counter -= 1;
+    }
+
+    fib_sequence
 }
+
+pub fn example() {}
