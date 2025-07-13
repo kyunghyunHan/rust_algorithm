551235db65e7669706ed0b81e2865bf4b3dce0f3
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-07-13 22:08:57 +1000

    feat:is_even

---
+++ src/algorithms/math/bits/is_even.rs
@@ -0,0 +1,3 @@
+pub fn is_even(number: u32) -> bool {
+    (number & 1) == 0
+}
--- src/algorithms/math/bits/mod.rs
+++ src/algorithms/math/bits/mod.rs
@@ -1,5 +1,6 @@
 pub mod clear_bit;
 pub mod get_bit;
+pub mod is_even;
 pub mod set_bit;
 pub mod update_bit;
 pub fn example() {}
