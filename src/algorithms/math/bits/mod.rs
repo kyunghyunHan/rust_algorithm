7d8b6b824cf40f5e2fc03078b620c81f65eadfff
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-07-17 22:33:23 +1000

    feat:update

--- src/algorithms/math/bits/mod.rs
+++ src/algorithms/math/bits/mod.rs
@@ -4,6 +4,7 @@ pub mod get_bit;
 pub mod is_even;
 pub mod is_positive;
 pub mod multiply_by_two;
+pub mod multiply_two_signed_numbers;
 pub mod set_bit;
 pub mod switch_sign;
 pub mod update_bit;
---
+++ src/algorithms/math/bits/multiply_two_signed_numbers.rs
@@ -0,0 +1,7 @@
+pub fn multiply_two_signed_numbers(a: u32, b: u32) -> u32 {
+    if b == 0 || a == 0 {
+        return 0;
+    }
+
+    0
+}
