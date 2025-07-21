cd34c04a9ec7096dd8870f42e6285879c936e62a
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-07-21 16:36:38 +1000

    feat:count_set_bits

--- src/algorithms/math/bits/count_set_bits.rs
+++ src/algorithms/math/bits/count_set_bits.rs
@@ -1,3 +1,13 @@
-pub fn count_set_bits(){
-    
+pub fn count_set_bits(original_number: u32) -> u32 {
+    let mut set_bits_count = 0;
+    let mut number = original_number;
+
+    while number != 0 {
+        // Add the last bit of the number to the sum of set bits
+        set_bits_count += number & 1;
+        // Unsigned right shift by one bit
+        number >>= 1;
+    }
+
+    set_bits_count
 }
