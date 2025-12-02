94e63df154bbb953a8ab78dd944c2c5284380889
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-12-02 22:06:57 +1100

    feat:update

--- src/algorithms/math/calculus/mod.rs
+++ src/algorithms/math/calculus/mod.rs
@@ -4,3 +4,15 @@ fn ln() {
     let x = 10.0_f64;
     println!("ln(10) = {}", x.ln());
 }
+pub type real = f64;
+
+pub fn diff_second<F>(f: F, x: real, h: real) -> real
+where
+    F: Fn(real) -> real,
+{
+    (f(x + h) - 2.0 * f(x) + f(x - h)) / (h * h)
+}
+
+fn example() {
+    let f = |x: real| x * x * x;
+}
