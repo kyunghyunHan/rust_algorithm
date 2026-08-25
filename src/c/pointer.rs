08005c34099ed88f71ba5204eaefb84fa7ea35d1
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-08-25 12:30:39 +0900

    feat:pointer

--- src/c/pointer.rs
+++ src/c/pointer.rs
@@ -3,13 +3,29 @@ pub fn example() {
     // let b = &mut a;
     // *b = 30;
     // println!("{}", a);
-    let mut resp: *mut i32;
-    let ( a, b) = (10, 20);
-    resp = sum( a,  b);
-    println!("{:?}",resp);
-    unsafe{
-        println!("{}",*resp);
+    // let mut resp: *mut i32;
+    // let ( a, b) = (10, 20);
+    // resp = sum( a,  b);
+    // println!("{:?}",resp);
+    // unsafe{
+    //     println!("{}",*resp);
+    // }
+
+    let mut a = 10;
+    let mut pi = &mut a;
+    let mut ppi = &mut pi;
+     **ppi = 20;
+    println!("{}",a);
+
+    let mut c = 10;
+    let mut d :*mut i32 = &mut c;
+    let mut e: *mut *mut i32  =&mut d;
+    unsafe {
+        **e = 100;
+        println!("{}",*d);
     }
+    
+
     
 }
 static mut res: i32 = 0;
