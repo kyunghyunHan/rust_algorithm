e6ea1a4959cdae5456bd5e36544aa003052711f7
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-09-11 23:43:47 +0900

    update

--- src/c/recursion/mod.rs
+++ src/c/recursion/mod.rs
@@ -1,40 +1,63 @@
-//
 const N: i32 = 5;
-fn func05(l: i32) {
-    if l > N {
-        return;
-    }
-    println!("{}", l);
-    func05(l + 1);
-    println!("{}", l);
-}
-fn func04(l: i32) {
-    if l > N {
-        return;
-    }
+static mut arr: [i32; 3] = [0; 3];
+/*
+1 2 3 4 5 출력
 
-    func04(l + 1);
-    println!("{}", l);
-}
-fn func03(l: i32) {
-    if l == 0 {
+*/
+fn test01(l: i32) {
+    if l > N {
         return;
     }
-    func03(l / 2);
-    println!("{}", l % 2);
+    print!("{} ", l);
+    test01(l + 1);
 }
-fn func02() {}
-fn print_binary(n: u32) {
-    if n == 0 {
+/*1 2 3 4 5 5 4 3 2 1 */
+fn test02(l: i32) {
+    if l > N {
         return;
     }
-    print_binary(n / 2);
-    println!("{}", n % 2);
+    print!("{} ", l);
+    test02(l + 1);
+    print!("{} ", l);
 }
-fn func01(l: i32) {
-    println!("{}", l);
-    func01(l + 1);
+/*1 2 3 4 5 4 3 2 1 */
+fn test03(l: i32) {
+    if l > N - 1 {
+        print!("{} ", l);
+        return;
+    }
+    print!("{} ", l);
+    test03(l + 1);
+    print!("{} ", l);
+}
+/*1 2 3 4 5 1 2 3 4*/
+fn test04(l: i32) {
+    if l > N {
+        return;
+    }
+    print!("{} ", l);
+    test04(l + 1);
+    print!("{} ", N - l + 1);
+}
+/*
+1 1 1
+1 1 2
+1 1 3
+1 2 1
+*/
+fn test05(l: i32) {
+    unsafe {
+        if l == 3 {
+            println!("{} {} {}", arr[0], arr[1], arr[2]);
+            return;
+        }
+
+        for i in 1..=3 {
+            arr[l as usize] = i;
+            test05(l + 1);
+        }
+    }
 }
 pub fn example() {
-    func05(1);
+    test05(0);
 }
