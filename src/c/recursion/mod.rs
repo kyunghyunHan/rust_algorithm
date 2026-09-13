65f938e4efe6c6d951fc5b8014f09189e309d892
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-09-13 23:16:15 +0900

    update

--- src/c/recursion/mod.rs
+++ src/c/recursion/mod.rs
@@ -1,5 +1,5 @@
 const N: i32 = 5;
-static mut arr: [i32; 3] = [0; 3];
+static mut ARR: [i32; 3] = [0; 3];
 /*
 1 2 3 4 5 출력
 
@@ -48,12 +48,12 @@ fn test04(l: i32) {
 fn test05(l: i32) {
     unsafe {
         if l == 3 {
-            println!("{} {} {}", arr[0], arr[1], arr[2]);
+            println!("{} {} {}", ARR[0], ARR[1], ARR[2]);
             return;
         }
 
         for i in 1..=3 {
-            arr[l as usize] = i;
+            ARR[l as usize] = i;
             test05(l + 1);
         }
     }
@@ -64,14 +64,14 @@ fn test05(l: i32) {
 fn test06(l: i32) {
     unsafe {
         if l == 3 {
-            println!("{} {} {}", arr[0], arr[1], arr[2]);
+            println!("{} {} {}", ARR[0], ARR[1], ARR[2]);
             return;
         }
         for i in 1..=6 {
-            if l > 0 && arr[l as usize - 1] >= i {
+            if l > 0 && ARR[l as usize - 1] >= i {
                 continue;
             }
-            arr[l as usize] = i;
+            ARR[l as usize] = i;
             test06(l + 1);
         }
     }
@@ -81,19 +81,22 @@ fn test07(l: i32, mut sum: i32) {
     unsafe {
         if l == 3 {
             if sum == 10 {
-                println!("{} {} {}", arr[0], arr[1], arr[2]);
+                println!("{} {} {}", ARR[0], ARR[1], ARR[2]);
             }
             return;
         }
         for i in 1..=6 {
-            if l > 0 && arr[l as usize - 1] >= i {
+            if l > 0 && ARR[l as usize - 1] >= i {
                 continue;
             }
-            arr[l as usize] = i;
+            ARR[l as usize] = i;
             test07(l + 1, sum + i);
         }
     }
 }
 pub fn example() {
+    let a: u8 = 0;
+
+    println!("{}", a.wrapping_sub(1));
     test07(0, 0);
 }
