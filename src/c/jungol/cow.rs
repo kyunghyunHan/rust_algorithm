4efd159b9bc6aa1067657c1fc64b004e2b131d01
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-09-22 14:37:48 +0900

    update

--- src/c/jungol/cow.rs
+++ src/c/jungol/cow.rs
@@ -1,5 +1,18 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+fn binary_search(arr: Vec<i32>, mut left: i32, mut right: i32, target: i32, upper: i32) -> i32 {
+    while (left < right) {
+        let mid = left + (right - left) / 2;
+
+        if arr[mid as usize] > target || (upper == 0 && arr[mid as usize] == target) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
 pub fn example() {
     let mut cnt = 0;
     let file = File::open("data.txt").unwrap();
@@ -31,22 +44,28 @@ pub fn example() {
             let min = arr[j as usize] + d1;
             let max = arr[j as usize] + d1 * 2;
             //세번쨰 소는 반드시 j보다 오른쪽
-            if lower < j + 1 {
-                lower = j + 1;
-            }
-            //min이상인 값이 처음나오는 위치
-            while lower < n && arr[lower as usize] < min {
-                lower += 1;
-            }
-            //upper은 lower보다 왼쪽에 있으면안댐
-            if upper < lower {
-                upper = lower;
-            }
-            //max이상인 값이 처음나오는 위치
-            while upper < n && arr[upper as usize] <= max {
-                upper += 1;
-            }
-            cnt += upper - lower;
+
+            let first = binary_search(arr.clone(), j + 1, n, min, 0);
+            let last = binary_search(arr.clone(), j + 1, n, max, 1);
+
+            cnt += last - first;
+
+            // if lower < j + 1 {
+            //     lower = j + 1;
+            // }
+            // //min이상인 값이 처음나오는 위치
+            // while lower < n && arr[lower as usize] < min {
+            //     lower += 1;
+            // }
+            // //upper은 lower보다 왼쪽에 있으면안댐
+            // if upper < lower {
+            //     upper = lower;
+            // }
+            // //max이상인 값이 처음나오는 위치
+            // while upper < n && arr[upper as usize] <= max {
+            //     upper += 1;
+            // }
+            // cnt += upper - lower;
         }
     }
     println!("{}", cnt);
